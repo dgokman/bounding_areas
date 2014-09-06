15511e6fa139b0fd1157cc284e63f08fe67bf99c
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-06 11:14:33 -0400

    created boundingbox class

--- lib/bounding_box.rb
+++ lib/bounding_box.rb
@@ -1,2 +1,37 @@
 class BoundingBox
+  def initialize(left, bottom, width, height)
+    @left = left
+    @bottom = bottom
+    @width = width
+    @height = height
+  end
+
+  def width
+    @width
+  end
+
+  def height
+    @height
+  end
+
+  def left
+    @left
+  end
+
+  def right
+    @left + @width
+  end
+
+  def top
+    @bottom + @height
+  end
+
+  def bottom
+    @bottom
+  end
+
+  def contains_point?(x, y)
+    x >= left && x <= right && y >= bottom && y <= top
+  end
+
 end
