ad18bc42c92171735a7373283a00e1138ce99b07
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-06 20:56:32 -0400

    passed 2nd test for bounding area

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,25 +1,76 @@
 require 'pry'
 require_relative 'bounding_box'
-class BoundingArea
-  def initialize(area)
-    bottom_rect = nil
-    top_rect = nil
-    @bounding_area = [bottom_rect, top_rect]
+
+class BoundingBox
+  def initialize(left, bottom, width, height)
+    @left = left
+    @bottom = bottom
+    @width = width
+    @height = height
   end
 
-  def bounding_area
-    @bounding_area
+  def width
+    @width
+  end
+
+  def height
+    @height
+  end
+
+  def left
+    @left
+  end
+
+  def right
+    @left + @width
+  end
+
+  def top
+    @bottom + @height
+  end
+
+  def bottom
+    @bottom
   end
 
   def contains_point?(x, y)
-    if bounding_area == [nil, nil]
-      return false
-    end
+    x >= left && x <= right && y >= bottom && y <= top
   end
+end
+
+
+class BoundingArea
+  def initialize(rects = [])
+    @rects = rects
+  end
+
+  def rects
+    @rects
+  end
+
+  def contains_point?(x, y)
+    if rects == []
+      return false
+    else
+      rects.each do |rect|
+        if x >= rect.left && x <= rect.right && y >= rect.bottom && y <= rect.top
+        return true
+        end
+      rect
+      end
+    end
+
+  end
+
+
+
 
 end
 
-# area = []
-# a = BoundingArea.new(area)
-# a.contains_point?(0,0)
+# bottom_rect = BoundingBox.new(0.0, 0.0, 2.0, 1.0)
+# top_rect = BoundingBox.new(2.0, 1.0, 3.0, 4.0)
 
+# area = BoundingArea.new([bottom_rect, top_rect])
+
+# area.contains_point?(0.5,0.5)
+#binding.pry
